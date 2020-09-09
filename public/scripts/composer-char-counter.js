c3fd769c447074cccaaa980a76a52ade653b74dd
vagrant <maham.16491@gmail.com>
2020-09-09 21:36:14 +0000

    char count, tweet container, fixed nav

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.new-tweet textarea').on('keyup', function() {
+
+    let counter = 140 - $(this).val().length;
+
+    $(this).siblings('div').children('.counter').text(counter);
+
+    if (counter < 0) {
+      $(this).siblings('div').children('.counter').css('color', 'red');
+    }
+
+  });
+}); 
